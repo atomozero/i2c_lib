cc5604f71d8a0a07bff4503b477b258d24157843
atomozero <bernardi.andrea@gmail.com>
2024-10-01 23:09:34 +0200

    Update i2c_driver.cpp

--- i2c_driver.cpp
+++ i2c_driver.cpp
@@ -1,152 +1,120 @@
 #include <KernelExport.h>
-#include <drivers/Drivers.h>
-#include <fcntl.h>
+#include <Drivers.h>
+#include <Errors.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
-#define I2C_DEVICE_PATH "i2c/test"
+#define I2C_DEVICE_PATH "i2c/my_i2c_device"
+#define I2C_SET_SLAVE 0x0703
 
-// Definizione di alcuni comandi base I2C
-#define I2C_READ  0x01
-#define I2C_WRITE 0x00
+// Funzioni Hook del driver
+status_t i2c_open(const char* name, uint32 flags, void** cookie);
+status_t i2c_close(void* cookie);
+status_t i2c_read(void* cookie, off_t pos, void* buffer, size_t* length);
+status_t i2c_write(void* cookie, off_t pos, const void* buffer, size_t* length);
+status_t i2c_ioctl(void* cookie, uint32 op, void* buffer, size_t length);
 
-// Struttura del dispositivo I2C
-typedef struct {
-    int bus_number;
-    bool active;
-} i2c_device;
-
-// Dichiarazioni delle funzioni del driver
-static status_t i2c_open(const char* name, uint32 flags, void** cookie);
-static status_t i2c_close(void* cookie);
-static status_t i2c_read(void* cookie, off_t pos, void* buffer, size_t* length);
-static status_t i2c_write(void* cookie, off_t pos, const void* buffer, size_t* length);
-static status_t i2c_ioctl(void* cookie, uint32 op, void* buffer, size_t length);
-
-// Struttura con le funzioni del driver
+// Tabella dei "device hooks"
 static device_hooks i2c_hooks = {
     &i2c_open,
     &i2c_close,
     &i2c_ioctl,
     &i2c_read,
     &i2c_write,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
+    NULL,  // No control hook
+    NULL,  // No select hook
+    NULL   // No deselect hook
 };
 
-// Funzione di inizializzazione del driver
-status_t
-init_hardware() {
-    // Funzione chiamata al caricamento del driver per fare un check dell'hardware
-    dprintf("i2c_driver: init_hardware()\n");
+// Funzione di apertura del dispositivo
+status_t i2c_open(const char* name, uint32 flags, void** cookie) {
+    dprintf("i2c_driver: i2c_open() - Nome dispositivo: %s\n", name);
+    *cookie = NULL; // Non usiamo il "cookie" in questo esempio
     return B_OK;
 }
 
-// Funzione chiamata quando il driver viene inizializzato
-status_t
-init_driver() {
-    dprintf("i2c_driver: init_driver()\n");
-
-    // Aggiunge il dispositivo I2C al filesystem di Haiku
-    if (devfs_publish_device(I2C_DEVICE_PATH, NULL, &i2c_hooks)) {
-        dprintf("i2c_driver: impossibile registrare il dispositivo %s\n", I2C_DEVICE_PATH);
-        return B_ERROR;
-    }
-
-    return B_OK;
-}
-
-// Funzione chiamata per chiudere il driver
-void
-uninit_driver() {
-    dprintf("i2c_driver: uninit_driver()\n");
-
-    // Rimuove il dispositivo I2C dal filesystem di Haiku
-    devfs_unpublish_device(I2C_DEVICE_PATH, NULL);
-}
-
-// Funzione di apertura del dispositivo I2C
-static status_t
-i2c_open(const char* name, uint32 flags, void** cookie) {
-    dprintf("i2c_driver: i2c_open()\n");
-
-    // Alloca memoria per il dispositivo e inizializza i campi
-    i2c_device* device = (i2c_device*)malloc(sizeof(i2c_device));
-    device->bus_number = 0;  // Imposta il bus I2C 0
-    device->active = true;
-    *cookie = device;
-
-    return B_OK;
-}
-
-// Funzione di chiusura del dispositivo I2C
-static status_t
-i2c_close(void* cookie) {
+// Funzione di chiusura del dispositivo
+status_t i2c_close(void* cookie) {
     dprintf("i2c_driver: i2c_close()\n");
-
-    // Dealloca la memoria del dispositivo
-    i2c_device* device = (i2c_device*)cookie;
-    free(device);
-
     return B_OK;
 }
 
-// Funzione di lettura da un dispositivo I2C
-static status_t
-i2c_read(void* cookie, off_t pos, void* buffer, size_t* length) {
+// Funzione di lettura dal dispositivo
+status_t i2c_read(void* cookie, off_t pos, void* buffer, size_t* length) {
     dprintf("i2c_driver: i2c_read() - Posizione: %lld, Lunghezza: %zu\n", pos, *length);
-
-    // Simula una lettura, puoi sostituire questo blocco con codice I2C reale
-    uint8_t* read_buffer = (uint8_t*)buffer;
-    for (size_t i = 0; i < *length; i++) {
-        read_buffer[i] = i;  // Dati di test
-    }
-
+    memset(buffer, 0, *length); // Legge dati fittizi (tutti 0)
     return B_OK;
 }
 
-// Funzione di scrittura su un dispositivo I2C
-static status_t
-i2c_write(void* cookie, off_t pos, const void* buffer, size_t* length) {
+// Funzione di scrittura sul dispositivo
+status_t i2c_write(void* cookie, off_t pos, const void* buffer, size_t* length) {
     dprintf("i2c_driver: i2c_write() - Posizione: %lld, Lunghezza: %zu\n", pos, *length);
-
-    // Simula una scrittura, puoi sostituire questo blocco con codice I2C reale
     const uint8_t* write_buffer = (const uint8_t*)buffer;
     for (size_t i = 0; i < *length; i++) {
         dprintf("i2c_driver: Dato scritto %02x\n", write_buffer[i]);
     }
-
     return B_OK;
 }
 
-// Funzione di gestione di operazioni specifiche con ioctl
-static status_t
-i2c_ioctl(void* cookie, uint32 op, void* buffer, size_t length) {
+// Funzione di gestione delle operazioni speciali (ioctl)
+status_t i2c_ioctl(void* cookie, uint32 op, void* buffer, size_t length) {
     dprintf("i2c_driver: i2c_ioctl() - Operazione: %u\n", op);
-
-    // Aggiungi qui la gestione di operazioni specifiche come impostazione della velocità, ecc.
     switch (op) {
         case I2C_SET_SLAVE:
             dprintf("i2c_driver: I2C_SET_SLAVE chiamato\n");
-            // Gestione dell'impostazione del dispositivo slave
+            // Imposta il dispositivo slave I2C (simulato)
             break;
         default:
-            return B_DEV_INVALID_IOCTL;
+            dprintf("i2c_driver: Operazione non supportata\n");
+            return B_BAD_VALUE;
     }
-
     return B_OK;
 }
 
-// Dichiarazione delle funzioni principali del driver
-const char** publish_devices() {
-    static const char* devices[] = { I2C_DEVICE_PATH, NULL };
-    return devices;
+// Funzione di inizializzazione dell'hardware (opzionale)
+status_t init_hardware() {
+    dprintf("i2c_driver: init_hardware()\n");
+    return B_OK;
 }
 
-device_hooks* find_device(const char* name) {
-    return &i2c_hooks;
+// Funzione di inizializzazione del driver
+status_t init_driver() {
+    dprintf("i2c_driver: init_driver()\n");
+    if (publish_device(I2C_DEVICE_PATH, &i2c_hooks) != B_OK) {
+        dprintf("i2c_driver: impossibile registrare il dispositivo %s\n", I2C_DEVICE_PATH);
+        return B_ERROR;
+    }
+    return B_OK;
 }
+
+// Funzione di disinstallazione del driver
+void uninit_driver() {
+    dprintf("i2c_driver: uninit_driver()\n");
+    unpublish_device(I2C_DEVICE_PATH);
+}
+
+// Funzione di gestione dei comandi per il modulo del kernel
+status_t i2c_std_ops(int32 op, ...) {
+    switch (op) {
+        case B_MODULE_INIT:
+            return init_driver();
+        case B_MODULE_UNINIT:
+            uninit_driver();
+            return B_OK;
+        default:
+            return B_BAD_VALUE;
+    }
+}
+
+// Definizione del modulo driver
+module_info i2c_driver_module = {
+    "drivers/i2c/my_i2c_device",
+    0,
+    i2c_std_ops
+};
+
+// Esportazione del modulo
+_EXPORT module_info* modules[] = {
+    &i2c_driver_module,
+    NULL
+};
