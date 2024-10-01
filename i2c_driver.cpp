55d6a9ad1270b03447cbbaa97cc39495f53437c2
atomozero <bernardi.andrea@gmail.com>
2024-10-01 23:37:15 +0200

    Update i2c_driver.cpp

--- i2c_driver.cpp
+++ i2c_driver.cpp
@@ -20,12 +20,12 @@ status_t i2c_ioctl(void* cookie, uint32 op, void* buffer, size_t length);
 static device_hooks i2c_hooks = {
     &i2c_open,
     &i2c_close,
-    &i2c_ioctl,
-    &i2c_read,
-    &i2c_write,
-    NULL,  // No control hook
-    NULL,  // No select hook
-    NULL   // No deselect hook
+    &i2c_ioctl,  // Ioctl è trattato come "control" hook, quindi il tipo corretto è "device_control_hook"
+    &i2c_read,   // Read hook
+    &i2c_write,  // Write hook
+    NULL,        // No free hook
+    NULL,        // No select hook
+    NULL         // No deselect hook
 };
 
 // Funzione di apertura del dispositivo
@@ -43,14 +43,14 @@ status_t i2c_close(void* cookie) {
 
 // Funzione di lettura dal dispositivo
 status_t i2c_read(void* cookie, off_t pos, void* buffer, size_t* length) {
-    dprintf(STDOUT_FILENO, "i2c_driver: i2c_read() - Posizione: %lld, Lunghezza: %zu\n", pos, *length);
+    dprintf(STDOUT_FILENO, "i2c_driver: i2c_read() - Posizione: %ld, Lunghezza: %zu\n", pos, *length);
     memset(buffer, 0, *length); // Legge dati fittizi (tutti 0)
     return B_OK;
 }
 
 // Funzione di scrittura sul dispositivo
 status_t i2c_write(void* cookie, off_t pos, const void* buffer, size_t* length) {
-    dprintf(STDOUT_FILENO, "i2c_driver: i2c_write() - Posizione: %lld, Lunghezza: %zu\n", pos, *length);
+    dprintf(STDOUT_FILENO, "i2c_driver: i2c_write() - Posizione: %ld, Lunghezza: %zu\n", pos, *length);
     const uint8_t* write_buffer = (const uint8_t*)buffer;
     for (size_t i = 0; i < *length; i++) {
         dprintf(STDOUT_FILENO, "i2c_driver: Dato scritto %02x\n", write_buffer[i]);
@@ -82,7 +82,7 @@ status_t init_hardware() {
 // Funzione di inizializzazione del driver
 status_t init_driver() {
     dprintf(STDOUT_FILENO, "i2c_driver: init_driver()\n");
-    if (publish_device(I2C_DEVICE_PATH, &i2c_hooks) != B_OK) {
+    if (publish_devices(I2C_DEVICE_PATH, &i2c_hooks) != B_OK) {
         dprintf(STDOUT_FILENO, "i2c_driver: impossibile registrare il dispositivo %s\n", I2C_DEVICE_PATH);
         return B_ERROR;
     }
@@ -92,7 +92,7 @@ status_t init_driver() {
 // Funzione di disinstallazione del driver
 void uninit_driver() {
     dprintf(STDOUT_FILENO, "i2c_driver: uninit_driver()\n");
-    unpublish_devices(I2C_DEVICE_PATH);  // Correzione della funzione
+    publish_devices(I2C_DEVICE_PATH);  // Correzione della funzione
 }
 
 // Funzione di gestione dei comandi per il modulo del kernel
